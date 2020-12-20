fbd007c8b28ced115843c19c2ff59802ebd84114
rockshellS <spicer.rachelle@gmail.com>
2020-12-20 15:55:15 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 18, 'brown', 'playful', 'F', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'black', 'joyful', 'F', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'blue','fearful', 'M', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'green','empathetic','F', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'yellow', 'joyful', 'M', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'red', 'playful', 'F', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'purple','playful', 'F', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 18, 'black', 'empathetic', 'F', 1);
+
